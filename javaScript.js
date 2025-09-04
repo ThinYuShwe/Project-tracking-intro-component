62412d69565000c40ae102249bee8376434f7daf
Thin Yu <142289690+ThinYuShwe@users.noreply.github.com>
2025-09-04 23:28:21 +0630

    javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -13,3 +13,14 @@ closeIcon.addEventListener("click", () => {
   dropDownBox.style.display = "none";
   hamburger.style.display = "block";
 });
+
+window.addEventListener("resize", () => {
+  const minWidth = 75 * parseFloat(getComputedStyle(document.documentElement).fontSize); 
+  // 75rem converted to px
+
+  if (window.innerWidth >= minWidth) {
+    dropDownBox.style.display = "none";
+    hamburger.style.display = "none";
+    closeIcon.style.display = "none";
+  }
+});
